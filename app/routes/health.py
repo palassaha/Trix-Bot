4cb3b7e8214742d80a6cfb4b38d25068c4a49aa1
N-Thander <nasiruddinthander2002@gmail.com>
2025-02-19 02:45:09 +0530

    ci cd implementation

--- app/routes/health.py
+++ app/routes/health.py
@@ -1,1 +1,47 @@
 
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from app.db.supabase import sb
+from app.core.config import settings
+
+import httpx
+
+router = APIRouter()
+
+@router.get("/health")
+async def health():
+    try:
+        db_response = sb.get_client().table("users").select("count", count='exact').execute()
+        
+        async with httpx.AsyncClient() as client:
+            ollama_resp = await client.get(f"{settings.OLLAMA_HOST}/")
+            
+        
+        return JSONResponse(
+            status_code=200,
+            content={
+                "status": "healty",
+                "database": "connected",
+                "version": "0.0.1",
+                "sevices": {
+                    "database": True,
+                    "ollama": True
+                }           
+            }
+        )
+        
+    except Exception as e:
+        return JSONResponse(
+            status_code=500,
+            content={
+                "status": "healty",
+                "database": "disconnected",
+                "version": "0.0.1",
+                "sevices": {
+                    "database": False,
+                    "ollama": False
+                }           
+            }
+        )
+        
+        
